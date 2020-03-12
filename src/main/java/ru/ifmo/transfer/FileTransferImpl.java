2e7c08b56721b9002856c8da1a7b225bb871e6d0
Aroize <gipermonk@bk.ru>
2020-03-12 12:19:10 +0300

    implemented file transfer

---
+++ src/main/java/ru/ifmo/transfer/FileTransferImpl.java
@@ -0,0 +1,47 @@
+package ru.ifmo.transfer;
+
+import ru.ifmo.reader.FileReader;
+import ru.ifmo.reader.FileReaderImpl;
+import ru.ifmo.writer.FileWriter;
+import ru.ifmo.writer.FileWriterImpl;
+
+import java.io.IOException;
+
+/**
+ * Created on 12.03.2020
+ *
+ * @author Ilia Ilmenskii
+ * Project : SampleProject
+ */
+public class FileTransferImpl implements FileTransfer {
+    @Override
+    public boolean transfer(String inFile, String outFile) {
+        FileReader reader = new FileReaderImpl();
+        FileWriter writer = new FileWriterImpl();;
+
+        try {
+            boolean isOpened = reader.open(inFile);
+            if (!isOpened) {
+                System.out.println("Cannot open file " + inFile);
+                return false;
+            }
+            isOpened = writer.open(outFile);
+            if (!isOpened) {
+                System.out.println("Cannot open file " + outFile);
+                return false;
+            }
+            int read = 0;
+            do {
+                read = reader.read();
+                if (read > 0)
+                    writer.write(read);
+            } while (read > 0);
+            return true;
+        } catch (IOException e) {
+            return false;
+        } finally {
+            reader.close();
+            writer.close();
+        }
+    }
+}
