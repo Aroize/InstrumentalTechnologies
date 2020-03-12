77c9bccf0ad0bc578f41f9768de0a61cf9744855
Aroize <gipermonk@bk.ru>
2020-03-12 12:14:54 +0300

    added file transfer inteface

---
+++ src/main/java/ru/ifmo/transfer/FileTransfer.java
@@ -0,0 +1,11 @@
+package ru.ifmo.transfer;
+
+/**
+ * Created on 12.03.2020
+ *
+ * @author Ilia Ilmenskii
+ * Project : SampleProject
+ */
+public interface FileTransfer {
+    boolean transfer(String inFile, String outFile);
+}
